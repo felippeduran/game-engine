2c9f801a9fc2a742b30b8b0e63e1fc3cee44b85c
Felippe Durán <felippe.duran@tfgco.com>
2017-02-03 01:16:07 -0200

    Initial Commit

---
+++ GameEngine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameEngine
+//
+//  Created by Felippe Durán on 2/3/17.
+//  Copyright © 2017 Felippe Durán. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
