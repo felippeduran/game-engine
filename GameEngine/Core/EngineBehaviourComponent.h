4edd5b39aad3d729132fd287d448cf8a065a950d
Felippe Durán <felippe.duran@tfgco.com>
2017-03-19 20:20:19 -0300

    Add EngineBehaviourComponent and EngineTransformComponent.

---
+++ GameEngine/Core/EngineBehaviourComponent.h
@@ -0,0 +1,34 @@
+//
+//  EngineBehaviourComponent.h
+//  GameEngine
+//
+//  Created by Felippe Durán on 3/14/17.
+//  Copyright © 2017 Felippe Durán. All rights reserved.
+//
+
+#ifndef EngineBehaviourComponent_h
+#define EngineBehaviourComponent_h
+
+#include "EngineObjectComponent.h"
+
+class EngineBehaviourComponent : public EngineObjectComponent {
+public:
+    EngineBehaviourComponent(EngineObject *object, EngineObjectPool *objectPool) : EngineObjectComponent(object, objectPool) {};
+    
+    void setEnabled(bool enabled);
+    bool isEnabled() const;
+    
+    virtual void onEnable();
+    virtual void onDisable();
+    
+    void onUnregistered() override final;
+    void onRegistered() override final;
+    
+private:
+    bool enabled;
+    
+    using EngineObjectComponent::registerComponent;
+    using EngineObjectComponent::unregisterComponent;
+};
+
+#endif /* EngineObjectBehaviour_h */
---
+++ GameEngine/Core/EngineTransformComponent.cpp
@@ -0,0 +1,9 @@
+//
+//  EngineTransformComponent.cpp
+//  GameEngine
+//
+//  Created by Felippe Durán on 3/14/17.
+//  Copyright © 2017 Felippe Durán. All rights reserved.
+//
+
+#include "EngineTransformComponent.h"
