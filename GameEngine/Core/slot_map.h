1b476d6129553bc76e106f2fdbe61c3a52bbe5fa
Felippe Durán <felippe.duran@tfgco.com>
2017-03-27 00:05:20 -0300

    Refactor slot_map to ensure no memory fragmentation.

--- GameEngine/Core/slot_map.h
+++ GameEngine/Core/slot_map.h
@@ -22,28 +22,48 @@ public:
                 freeList.push_back((int)objects.size() * (int)chunk_size + i);
             }
             objects.push_back(chunk);
+            indirection.resize(indirection.size() + chunk_size);
         }
         
         int free = freeList.back();
         freeList.pop_back();
-        return objects[free / chunk_size][free % chunk_size].id;
+        indirection[free] = count;
+        long long id = objects[count / chunk_size][count % chunk_size].id;
+        count += 1;
+        return id;
     };
     
     T* getObject(long long id) {
-        T* obj = objects[(id & 0xFFFFFFFF) / chunk_size] + ((id & 0xFFFFFFFF) % chunk_size);
+        int index = indirection[id & 0xFFFFFFFF];
+        T*obj = objects[index / chunk_size] + index % chunk_size;
         return obj->id != id ? nullptr : obj;
     };
     
     void destroyObject(long long id) {
         T* obj = getObject(id);
-        obj->id = (obj->id & 0xFFFFFFFF) | (((obj->id >> 32) + 1) << 32);
-        freeList.push_back(id & 0xFFFFFFFF);
+        if (obj) {
+            obj->id = (obj->id & 0xFFFFFFFF) | (((obj->id >> 32) + 1) << 32);
+            freeList.push_back(id & 0xFFFFFFFF);
+
+            int index = indirection[id & 0xFFFFFFFF];
+            int lastUsedIndex = count - 1;
+            T lastObj = objects[lastUsedIndex / chunk_size][lastUsedIndex % chunk_size];
+            
+            objects[lastUsedIndex / chunk_size][lastUsedIndex % chunk_size] = *obj;
+            objects[index / chunk_size][index % chunk_size] = lastObj;
+            
+            indirection[lastObj.id & 0xFFFFFFFF] = index;
+            
+            count -= 1;
+        }
     };
     
 private:
     std::vector<T *> objects;
+    std::vector<int> indirection;
     std::vector<int> freeList;
     const size_t chunk_size = 256;
+    int count;
 };
 
 #endif /* slot_map_h */
