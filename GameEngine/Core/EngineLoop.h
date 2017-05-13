ce2a9a41c5da180329292385c6d08f1fb330cd7f
Felippe Durán <felippe.duran@tfgco.com>
2017-05-13 18:01:28 -0300

    Clean up to start from scratch.

--- GameEngine/Core/EngineLoop.h
+++ GameEngine/Core/EngineLoop.h
@@ -9,54 +9,12 @@
 #ifndef EngineLoop_h
 #define EngineLoop_h
 
-#include <set>
-#include <vector>
-#include "slot_map.h"
-
-class EngineObject;
-class EngineObjectComponent;
-
 class EngineLoop {
 public:    
     void update(double dt);
     
-    object_id createObject();
-    EngineObject* getObject(object_id id);
-    void destroyObject(object_id id);
-    
-    template <class T> T *addComponent(EngineObject *object);
-    void destroyComponent(EngineObjectComponent *component);
-    
-    void registerComponent(EngineObjectComponent *component);
-    void unregisterComponent(EngineObjectComponent *component);
-    
 private:
-    std::set<object_id> destroyedObjects;
-    slot_map<EngineObject> objects;
     
-    std::set<EngineObjectComponent *> registeredComponents;
-    std::set<EngineObjectComponent *> registeringComponents;
-    std::set<EngineObjectComponent *> unregisteringComponents;
-    std::set<EngineObjectComponent *> destroyedComponents;
-    
-    void unregisterComponents();
-    void registerComponents();
-    
-    void updateRegisteredComponents(double dt);
-    void cleanupDestroyedObjects();
-    void cleanupDestroyedComponents();
-    void destroyObjects();
-    void destroyComponents();
 };
-
-
-#include "EngineObject.h"
-
-template <class T> T* EngineLoop::addComponent(EngineObject *object) {
-    static_assert(std::is_base_of<EngineObjectComponent, T>::value, "Template class is not a EngineObjectComponent!");
-    T *component = new T(object->getId(), this);
-    object->insertComponent(component);
-    return component;
-}
 
 #endif /* EngineLoop_h */
--- GameEngine/Core/EngineTransformComponent.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  EngineTransformComponent.cpp
-//  GameEngine
-//
-//  Created by Felippe Durán on 3/14/17.
-//  Copyright © 2017 Felippe Durán. All rights reserved.
-//
-
-#include "EngineTransformComponent.h"
