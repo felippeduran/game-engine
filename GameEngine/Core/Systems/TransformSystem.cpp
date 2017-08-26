e4cbad38deec8f3112e2c176b0a4313b7387fd48
Felippe Durán <felippe.duran@tfgco.com>
2017-08-26 01:13:45 -0300

    Sort entities by parent order before updating transforms.

--- GameEngine/Core/Systems/TransformSystem.cpp
+++ GameEngine/Core/Systems/TransformSystem.cpp
@@ -10,42 +10,49 @@
 #include "Transform.h"
 #include <glm/gtx/transform.hpp>
 
+#include <iomanip>
+#include <set>
+
 using namespace entityx;
 using namespace glm;
 
 void logMat4(mat4& mat) {
     std::cout << "<mat4> " << &mat << std::endl;
-    std::cout << mat[0][0] << mat[0][1] << mat[0][2] << mat[0][3] << std::endl;
-    std::cout << mat[1][0] << mat[1][1] << mat[1][2] << mat[1][3] << std::endl;
-    std::cout << mat[2][0] << mat[2][1] << mat[2][2] << mat[2][3] << std::endl;
-    std::cout << mat[3][0] << mat[3][1] << mat[3][2] << mat[3][3] << std::endl;
+    std::cout << std::fixed << std::setprecision(1) << " " << (mat[0][0] < 0.0f ? "" : " ") << mat[0][0] << " " << (mat[0][1] < 0.0f ? "" : " ") << mat[0][1] << " " << (mat[0][2] < 0.0f ? "" : " ") << mat[0][2] << " " << (mat[0][3] < 0.0f ? "" : " ") << mat[0][3] << std::endl;
+    std::cout << std::fixed << std::setprecision(1) << " " << (mat[1][0] < 0.0f ? "" : " ") << mat[1][0] << " " << (mat[1][1] < 0.0f ? "" : " ") << mat[1][1] << " " << (mat[1][2] < 0.0f ? "" : " ") << mat[1][2] << " " << (mat[1][3] < 0.0f ? "" : " ") << mat[1][3] << std::endl;
+    std::cout << std::fixed << std::setprecision(1) << " " << (mat[2][0] < 0.0f ? "" : " ") << mat[2][0] << " " << (mat[2][1] < 0.0f ? "" : " ") << mat[2][1] << " " << (mat[2][2] < 0.0f ? "" : " ") << mat[2][2] << " " << (mat[2][3] < 0.0f ? "" : " ") << mat[2][3] << std::endl;
+    std::cout << std::fixed << std::setprecision(1) << " " << (mat[3][0] < 0.0f ? "" : " ") << mat[3][0] << " " << (mat[3][1] < 0.0f ? "" : " ") << mat[3][1] << " " << (mat[3][2] < 0.0f ? "" : " ") << mat[3][2] << " " << (mat[3][3] < 0.0f ? "" : " ") << mat[3][3] << std::endl;
 };
 
+void dfsUtil(Entity entity, std::set<Entity::Id>& explored, std::vector<Entity>& sorted) {
+    if (entity.valid()) {
+        explored.insert(entity.id());
+        Entity parent = entity.component<Transform>()->parent;
+        if (explored.find(parent.id()) == explored.end()) dfsUtil(parent, explored, sorted);
+        sorted.push_back(entity);
+    }
+}
+
+std::vector<Entity> topsort(const EntityManager::View<Transform>& entities) {
+    std::vector<Entity> sorted;
+    std::set<Entity::Id> explored;
+    for (Entity entity : entities) {
+        if (explored.find(entity.id()) == explored.end()) dfsUtil(entity, explored, sorted);
+    }
+    return sorted;
+}
+
 void TransformSystem::update(EntityManager &es, EventManager &events, TimeDelta dt) {
-    std::cout << "TransformSystem" << std::endl;
-    es.each<Transform>([dt] (Entity entity, Transform &transform) {
-        glm::mat4 translationMatrix = glm::translate(transform.localPosition);
-        glm::mat4 rotationMatrix = glm::mat4_cast(transform.localRotation);
-        glm::mat4 scalingMatrix = glm::scale(transform.localScale);
+    std::vector<Entity> entities = topsort(es.entities_with_components<Transform>());
+    for (Entity entity : entities) {
+        ComponentHandle<Transform> transform = entity.component<Transform>();
+        glm::mat4 translationMatrix = glm::translate(transform->localPosition);
+        glm::mat4 rotationMatrix = glm::mat4_cast(transform->localRotation);
+        glm::mat4 scalingMatrix = glm::scale(transform->localScale);
         glm::mat4 modelMatrix = translationMatrix * rotationMatrix * scalingMatrix;
         
         glm::mat4 parentTransformMatrix;
-        if (transform.parent.valid()) parentTransformMatrix = transform.parent.component<Transform>()->localToWorldMatrix;
-        transform.localToWorldMatrix = parentTransformMatrix * (modelMatrix);
-        
-        std::cout << "Entity:" << &entity << std::endl;
-        if (transform.parent.valid()) std::cout << "Parent:" << &transform.parent << std::endl;
-        std::cout << "Composing matrices:" << std::endl;
-        logMat4(translationMatrix);
-        logMat4(rotationMatrix);
-        logMat4(scalingMatrix);
-        
-        std::cout << std::endl << "Model matrix:" << std::endl;
-        logMat4(modelMatrix);
-        
-        std::cout << std::endl << "Local to world matrix:" << std::endl;
-        logMat4(transform.localToWorldMatrix);
-        
-        std::cout << std::endl;
-    });
+        if (transform->parent.valid()) parentTransformMatrix = transform->parent.component<Transform>()->localToWorldMatrix;
+        transform->localToWorldMatrix = parentTransformMatrix * (modelMatrix);
+    }
 };
