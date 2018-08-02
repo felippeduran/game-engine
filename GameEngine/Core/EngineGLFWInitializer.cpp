e7ee3886133d4404ebd0139f084319c468592930
Felippe Durán <felippe.duran@tfgco.com>
2018-08-01 23:54:53 -0300

    Enable alpha blend.

--- GameEngine/Core/EngineGLFWInitializer.cpp
+++ GameEngine/Core/EngineGLFWInitializer.cpp
@@ -72,6 +72,8 @@ int EngineGLFWInitializer::initialize(InputHandler *inputHandler) {
     glViewport(0, 0, screenWidth, screenHeight);
     
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     
     return 0;
 }
