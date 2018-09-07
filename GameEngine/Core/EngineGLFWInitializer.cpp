f00e9b177aeb24ba4a8337a3899bffb354d80fa3
Felippe Durán <felippe.duran@tfgco.com>
2018-09-07 18:56:03 -0300

    Add imgui as a submodule.

--- GameEngine/Core/EngineGLFWInitializer.cpp
+++ GameEngine/Core/EngineGLFWInitializer.cpp
@@ -12,6 +12,10 @@
 #include <iostream>
 #include "InputHandler.h"
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
 using namespace std;
 
 static InputHandler *inputHandlerPtr = nullptr;
@@ -22,6 +26,7 @@ void cursorPosCallback(GLFWwindow* window, double xpos, double ypos);
 void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
 void scrollCallback(GLFWwindow* window, double xoffset, double yoffset);
 void cursorEnterCallback(GLFWwindow* window, int entered);
+void charCallback(GLFWwindow* window, unsigned int c);
 
 int EngineGLFWInitializer::initialize(InputHandler *inputHandler) {
     inputHandlerPtr = inputHandler;
@@ -53,6 +58,7 @@ int EngineGLFWInitializer::initialize(InputHandler *inputHandler) {
     glfwSetCursorPosCallback(window, cursorPosCallback);
     glfwSetScrollCallback(window, scrollCallback);
     glfwSetCursorEnterCallback(window, cursorEnterCallback);
+    glfwSetCharCallback(window, charCallback);
     
     glfwMakeContextCurrent(window);
     glewExperimental = GL_TRUE;
@@ -75,6 +81,17 @@ int EngineGLFWInitializer::initialize(InputHandler *inputHandler) {
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+    
+    ImGui_ImplGlfw_InitForOpenGL(window, false);
+    ImGui_ImplOpenGL3_Init("#version 410 core");
+    
+    // Setup style
+    ImGui::StyleColorsDark();
+    
     return 0;
 }
 
@@ -85,13 +102,24 @@ int EngineGLFWInitializer::runLoop(function<void (float)> updateCallback) {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        
         double time = glfwGetTime();
         updateCallback(time - lastTime);
         lastTime = time;
         
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+    
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
     
     glfwTerminate();
     return 0;
@@ -102,6 +130,7 @@ void errorCallback(int error, const char* description) {
 }
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    ImGui_ImplGlfw_KeyCallback(window, key, scancode, action, mods);
     inputHandlerPtr->keyCallback(window, key, scancode, action, mods);
 }
 
@@ -110,13 +139,19 @@ void cursorPosCallback(GLFWwindow* window, double xpos, double ypos) {
 }
 
 void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
+    ImGui_ImplGlfw_MouseButtonCallback(window, button, action, mods);
     inputHandlerPtr->mouseButtonCallback(window, button, action, mods);
 }
 
 void scrollCallback(GLFWwindow* window, double xoffset, double yoffset) {
+    ImGui_ImplGlfw_ScrollCallback(window, xoffset, yoffset);
     inputHandlerPtr->scrollCallback(window, xoffset, yoffset);
 }
 
 void cursorEnterCallback(GLFWwindow* window, int entered) {
     inputHandlerPtr->cursorEnterCallback(window, entered);
+}
+
+void charCallback(GLFWwindow* window, unsigned int c) {
+    ImGui_ImplGlfw_CharCallback(window, c);
 }
