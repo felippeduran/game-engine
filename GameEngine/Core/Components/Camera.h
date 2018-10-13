5c9ba21f1d0ca24873ec4a1eed910f2f130c8a89
Felippe Durán <felippe.duran@tfgco.com>
2018-10-13 16:20:27 -0300

    Replace Camera projection by field of view, aspect ration, and plane properties.

--- GameEngine/Core/Components/Camera.h
+++ GameEngine/Core/Components/Camera.h
@@ -12,9 +12,14 @@
 #include <glm/gtx/transform.hpp>
 
 struct Camera {
-    Camera(float fieldOfView = glm::radians(50.0f), float aspect = 800.0f/600.0f, float zNear = 0.1f, float zFar = 1000.0f) : projection(glm::perspective(fieldOfView, aspect, zNear, zFar)) {};
+    Camera(float fieldOfView = glm::radians(50.0f), float aspect = 800.0f/600.0f, float zNear = 0.1f, float zFar = 1000.0f) : fieldOfView(fieldOfView), aspectRatio(aspect), zNear(zNear), zFar(zFar) {};
     
-    glm::mat4 projection;
+    float fieldOfView;
+    float aspectRatio;
+    float zNear;
+    float zFar;
+    
+    glm::mat4 projection() { return glm::perspective(fieldOfView, aspectRatio, zNear, zFar); };
 };
 
 #endif /* Camera_h */
