cd13b6bcc872ed3b2591dbf9423033f7728e4377
Felippe Durán <felippe.duran@tfgco.com>
2019-03-04 13:32:24 -0300

    Refactor InspectorSystem to use reflection.

--- EngineEditor/Source/Systems/UI/InspectorSystem.cpp
+++ EngineEditor/Source/Systems/UI/InspectorSystem.cpp
@@ -10,111 +10,36 @@
 #include "Inspector.h"
 #include "imgui.h"
 #include "InspectorHighlight.h"
-
 #include "Name.h"
-#include "Transform.h"
-#include "MeshRenderer.h"
-#include "DirectionalLight.h"
-#include "Camera.h"
-
-#include "Mesh.h"
 #include "Material.h"
+#include <glm/gtc/quaternion.hpp>
+#include <rttr/type>
+#include "MetadataType.h"
 
 using namespace entityx;
 using namespace glm;
 using namespace GameEngine;
 using namespace GameEngine::Editor;
+using namespace rttr;
+using namespace std;
+
+void setupPropertiesFromVariant(type type, variant valueVariant);
+bool setupPropertyFromVariant(property property, variant valueVariant);
 
 void InspectorSystem::update(EntityManager &es, EventManager &events, TimeDelta dt) {
     es.each<Inspector>([this, &es, &events, dt] (Entity entity, Inspector &inspector) {
         if (ImGui::Begin("Inspector", NULL, 0)) {
             es.each<InspectorHighlight>([this, &es, &events, dt] (Entity entity, InspectorHighlight &highlight) {
                 ImGui::Text("%s", entity.component<Name>()->name.c_str());
-                
-                if (ImGui::CollapsingHeader("Transform")) {
-                    ComponentHandle<Transform> transform = entity.component<Transform>();
-                    float position[3] = { transform->localPosition.x, transform->localPosition.y, transform->localPosition.z };
-                    if (ImGui::InputFloat3("Position", position)) {
-                        transform->localPosition = vec3(position[0], position[1], position[2]);
-                    }
-                    
-                    vec3 rotation = glm::degrees(glm::eulerAngles(transform->localRotation));
-                    float localRotation[3] = { rotation.x, rotation.y, rotation.z };
-                    if (ImGui::InputFloat3("Rotation", localRotation)) {
-                        transform->localRotation = glm::quat(glm::radians(vec3(localRotation[0], localRotation[1], localRotation[2])));
-                    }
-                    
-                    float localScale[3] = { transform->localScale.x, transform->localScale.y, transform->localScale.z };
-                    if (ImGui::InputFloat3("Scale", localScale)) {
-                        transform->localScale = vec3(localScale[0], localScale[1], localScale[2]);
-                    }
-                }
-                
-                if (entity.has_component<MeshRenderer>() && ImGui::CollapsingHeader("Mesh Renderer")) {
-                    ImGui::BeginChild("MeshRendererChild");
-                    ComponentHandle<MeshRenderer> renderer = entity.component<MeshRenderer>();
-                    
-                    for (Material *material : renderer->mesh->materials) {
-                        std::string materialName = material->name + "##" + std::to_string((long)material);
-                        if (ImGui::CollapsingHeader(materialName.c_str())) {
-                            
-                            std::string ambientName = "Ambient Color##" + std::to_string((long)material);
-                            float ambient[3] = { material->colorAmbient.r, material->colorAmbient.g, material->colorAmbient.b };
-                            if (ImGui::InputFloat3(ambientName.c_str(), ambient)) {
-                                material->colorAmbient = vec3(ambient[0], ambient[1], ambient[2]);
+
+                for (type t : type::get_types()) {
+                    if(t.get_metadata(MetadataType::Component).is_valid()) {
+                        if(t.invoke("hasComponent", rttr::instance(), { entity }).get_value<bool>()) {
+                            variant componentVariant = t.invoke("retrieveComponent", rttr::instance(), { entity });
+                            if (ImGui::CollapsingHeader(((string)t.get_name()).c_str())) {
+                                setupPropertiesFromVariant(t, componentVariant);
                             }
-                            
-                            std::string diffuseName = "Diffuse Color##" + std::to_string((long)material);
-                            float diffuse[3] = { material->colorDiffuse.r, material->colorDiffuse.g, material->colorDiffuse.b };
-                            if (ImGui::InputFloat3(diffuseName.c_str(), diffuse)) {
-                                material->colorDiffuse = vec3(diffuse[0], diffuse[1], diffuse[2]);
-                            }
-                            
-                            std::string specularName = "Specular Color##" + std::to_string((long)material);
-                            float specular[3] = { material->colorSpecular.r, material->colorSpecular.g, material->colorSpecular.b };
-                            if (ImGui::InputFloat3(specularName.c_str(), specular)) {
-                                material->colorSpecular = vec3(specular[0], specular[1], specular[2]);
-                            }
-                            
-                            ImGui::PushItemWidth(100);
-                            ImGui::InputFloat("Dissolve", &material->dissolve);
-                            ImGui::InputFloat("Refraction Index", &material->refractionIndex);
-                            ImGui::InputFloat("Specular Exponent", &material->specularExponent);
-                            ImGui::PopItemWidth();
                         }
-                    }
-                    ImGui::EndChild();
-                }
-                
-                if (entity.has_component<Camera>() && ImGui::CollapsingHeader("Camera")) {
-                    ComponentHandle<Camera> camera = entity.component<Camera>();
-                    ImGui::PushItemWidth(100);
-                    ImGui::InputFloat("Field Of View", &camera->fieldOfView);
-                    ImGui::InputFloat("Aspect Ratio", &camera->aspectRatio);
-                    ImGui::InputFloat("Near Plane", &camera->zNear);
-                    ImGui::InputFloat("Far Plane", &camera->zFar);
-                    ImGui::PopItemWidth();
-                }
-                
-                if (entity.has_component<DirectionalLight>() && ImGui::CollapsingHeader("Directional Light")) {
-                    ComponentHandle<DirectionalLight> light = entity.component<DirectionalLight>();
-                    
-                    std::string ambientName = "Ambient Color##" + std::to_string((long)light);
-                    float ambient[3] = { light->ambient.r, light->ambient.g, light->ambient.b };
-                    if (ImGui::InputFloat3(ambientName.c_str(), ambient)) {
-                        light->ambient = vec3(ambient[0], ambient[1], ambient[2]);
-                    }
-                    
-                    std::string diffuseName = "Diffuse Color##" + std::to_string((long)light);
-                    float diffuse[3] = { light->diffuse.r, light->diffuse.g, light->diffuse.b };
-                    if (ImGui::InputFloat3(diffuseName.c_str(), diffuse)) {
-                        light->diffuse = vec3(diffuse[0], diffuse[1], diffuse[2]);
-                    }
-                    
-                    std::string specularName = "Specular Color##" + std::to_string((long)light);
-                    float specular[3] = { light->specular.r, light->specular.g, light->specular.b };
-                    if (ImGui::InputFloat3(specularName.c_str(), specular)) {
-                        light->specular = vec3(specular[0], specular[1], specular[2]);
                     }
                 }
             });
@@ -122,3 +47,50 @@ void InspectorSystem::update(EntityManager &es, EventManager &events, TimeDelta 
         ImGui::End();
     });
 };
+
+void setupPropertiesFromVariant(type type, variant valueVariant) {
+    for (auto& property : type.get_properties()) {
+        if (!setupPropertyFromVariant(property, valueVariant)) {
+            variant propertyVariant = property.get_value(valueVariant);
+            setupPropertiesFromVariant(property.get_type(), propertyVariant);
+        }
+    }
+}
+
+bool setupPropertyFromVariant(property property, variant valueVariant)
+{
+    bool evaluated = true;
+    variant propertyVariant = property.get_value(valueVariant);
+    if (property.get_type() == type::get<vec3>()) {
+        vec3 value = propertyVariant.get_value<vec3>();
+        float values[3] = { value.x, value.y, value.z };
+        if (ImGui::InputFloat3(((string)property.get_name()).c_str(), values)) {
+            property.set_value(valueVariant, vec3(values[0], values[1], values[2]));
+        }
+    } else if (property.get_type() == type::get<quat>()) {
+        vec3 value = glm::degrees(glm::eulerAngles(propertyVariant.get_value<quat>()));
+        float values[3] = { value.x, value.y, value.z };
+        if (ImGui::InputFloat3(((string)property.get_name()).c_str(), values)) {
+            property.set_value(valueVariant, glm::quat(glm::radians(vec3(values[0], values[1], values[2]))));
+        }
+    } if (property.get_type() == type::get<float>()) {
+        float value = propertyVariant.get_value<float>();
+        if (ImGui::InputFloat(((string)property.get_name()).c_str(), &value)) {
+            property.set_value(valueVariant, value);
+        }
+    } else if (propertyVariant.is_sequential_container()) {
+        variant_sequential_view seq_variant = propertyVariant.create_sequential_view();
+        for (int i = 0; i < seq_variant.get_size(); i++) {
+            variant variant = seq_variant.get_value(i).extract_wrapped_value();
+            if (variant.get_type() == type::get<Material *>()) {
+                string name = variant.get_value<Material *>()->name;
+                ImGui::BeginChild(((string)property.get_name()).c_str());
+                if (ImGui::CollapsingHeader(name.c_str())) {
+                    setupPropertiesFromVariant(variant.get_type(), variant);
+                }
+                ImGui::EndChild();
+            }
+        }
+    } else evaluated = false;
+    return evaluated;
+}
