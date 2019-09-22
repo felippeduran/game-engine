f7b3b2989bab34b123023298cc3d0c2c181ef620
Felippe Durán <felippe.duran@gmail.com>
2019-09-21 22:03:33 -0300

    Add EditorElement component.

---
+++ EngineEditor/Source/Components/EditorElement.h
@@ -0,0 +1,20 @@
+//
+//  EditorElement.h
+//  EngineEditor
+//
+//  Created by Felippe Durán on 04/03/19.
+//  Copyright © 2019 Felippe Durán. All rights reserved.
+//
+
+#ifndef EditorElement_h
+#define EditorElement_h
+
+namespace GameEngine {
+    namespace Editor {
+        struct EditorElement {
+            EditorElement() {};
+        };
+    };
+};
+
+#endif /* EditorElement_h */
